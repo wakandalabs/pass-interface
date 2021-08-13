c763bd491a75885f75bdd9465916d0465acab801
tunogya <tunogya@qq.com>
2021-08-13 13:59:08 +0800

    fix: format ChakraProvider with Providers

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,41 +1,25 @@
 import React from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../parts/ColorModeSwitcher';
 import { Logo } from '../parts/Logo';
+import Providers from "../global/providers.comp";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <Providers>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Logo/>
           </VStack>
         </Grid>
       </Box>
-    </ChakraProvider>
+    </Providers>
   );
 }
 
