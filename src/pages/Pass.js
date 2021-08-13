ead0fe5f80664999ae67e0de68499ae4ce6c4532
tunogya <tunogya@qq.com>
2021-08-13 21:58:07 +0800

    fix: component UserInfoCard.js

--- src/pages/Pass.js
+++ src/pages/Pass.js
@@ -1,41 +1,22 @@
 import React from 'react';
 import {
-  Avatar,
   Box,
-  Button,
   Heading,
-  Spacer,
   Stack,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
-  Text, useClipboard
 } from "@chakra-ui/react";
-import {SmallAddIcon} from "@chakra-ui/icons";
+import {UserInfoCard} from "../parts/UserInfoCard";
 
 function Pass() {
-  const {hasCopied, onCopy} = useClipboard("address")
-
   return (
     <Box pl={16} pr={16}>
       <Stack height={500} direction={"row"} mb={4}>
-        <Stack height={"100%"} width={"50%"} p={16} spacing={4}>
-          <Avatar bg="teal.500"/>
-          <Stack direction={"row"} align={"center"} spacing={2}>
-            <Heading fontSize="xl">@ID</Heading>
-            <Button size={"xs"} onClick={onCopy} width={36} colorScheme={"gray"} color={"gray"}>
-              {hasCopied ? "Copied!" : "0xaddressdddddd"}
-            </Button>
-          </Stack>
-
-          <Text>A collection of 1/1 Monsters by Des Lucréce</Text>
-          <Spacer/>
-          <Button leftIcon={<SmallAddIcon />}  width={28}>Follow</Button>
-        </Stack>
+        <UserInfoCard address={"99999999"}/>
         <Stack height={"100%"} width={"50%"}>
-
         </Stack>
       </Stack>
       <Stack>
---
+++ src/parts/UserInfoCard.js
@@ -0,0 +1,28 @@
+import {Avatar, Button, Heading, Spacer, Stack, Text, useClipboard} from "@chakra-ui/react";
+import {SmallAddIcon} from "@chakra-ui/icons";
+import React from "react";
+
+export function UserInfoCard({address}) {
+  const {hasCopied, onCopy} = useClipboard(address)
+
+  return (
+    <Stack height={"100%"} width={"50%"} p={16} spacing={4}>
+      <Avatar bg="teal.500"/>
+      <Stack direction={"row"} align={"center"} spacing={2}>
+        <Heading fontSize="xl">@ID</Heading>
+        <Button size={"xs"} onClick={onCopy} width={36} colorScheme={"gray"} color={"gray"}>
+          {hasCopied ? "Copied!" :  address }
+        </Button>
+      </Stack>
+      <Text>DESC</Text>
+      <Spacer/>
+      <Button leftIcon={<SmallAddIcon/>} width={28}>Follow</Button>
+    </Stack>
+  )
+}
+
+export default function WrappedUserInfoCard() {
+  return (
+    <UserInfoCard/>
+  )
+}
