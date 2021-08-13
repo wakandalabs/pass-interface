2e123c28d0d3d4012d35cec03d7caf2e801cb473
tunogya <tunogya@qq.com>
2021-08-13 13:59:45 +0800

    fix: format

--- src/parts/Logo.js
+++ src/parts/Logo.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Image } from '@chakra-ui/react';
 import logo from '../svg/logo.svg';
 
-
 export const Logo = props => {
   return <Image src={logo} {...props} />;
 };
