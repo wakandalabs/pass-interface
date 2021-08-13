45fd81d39d8333e64dfb98e7312e8c46713ada17
tunogya <tunogya@qq.com>
2021-08-13 20:43:40 +0800

    fix: add Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import {Box} from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Select
+} from "@chakra-ui/react";
+import {Search2Icon} from "@chakra-ui/icons";
 
 function Search() {
   return (
     <Box pl={16} pr={16}>
-      Search
+      <Box height={300}>
+        <Center>
+          Search
+        </Center>
+        <Center>
+        </Center>
+      </Box>
+      <Center mt={-7}>
+        <InputGroup width={"50%"} alignItems={"center"} h={14}>
+          <InputLeftElement
+            pointerEvents="none"
+            h={"100%"}
+            children={<Search2Icon color="gray.300" />}
+          />
+          <Input type="tel" placeholder="Search..." size="lg" bgColor={"white"} h={"100%"}/>
+          <InputRightElement children={
+            <Select variant="filled">
+              <option value="Pass">Pass</option>
+              <option value="User">User</option>
+            </Select>
+          } w={"auto"} h={"100%"} pr={2}/>
+        </InputGroup>
+      </Center>
+
+
     </Box>
   );
 }
