024b836485aface9c30dd68a7e3155565567c947
tunogya <tunogya@qq.com>
2021-08-15 16:35:31 +0800

    fix: rename

--- src/pages/Pass/UserInfoCard.js
+++ src/pages/Pass/UserInfoCard.js
@@ -9,14 +9,14 @@ export function UserInfoCard({address}) {
     <Stack height={"100%"} width={"50%"} p={16} spacing={4}>
       <Avatar bg="teal.500"/>
       <Stack direction={"row"} align={"center"} spacing={2}>
-        <Heading fontSize="xl">@ID</Heading>
+        <Heading fontSize="xl">Name</Heading>
         <Stack>
           <Button size={"xs"} onClick={onCopy} width={36} colorScheme={"gray"} color={"gray"}>
             {hasCopied ? "Copied!" :  address }
           </Button>
         </Stack>
       </Stack>
-      <Text>DESC</Text>
+      <Text>Info</Text>
       <Spacer/>
       <Button leftIcon={<SmallAddIcon/>} width={28}>Follow</Button>
     </Stack>
