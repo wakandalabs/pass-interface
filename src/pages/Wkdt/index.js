9bbc06a3b00fc36b42881f1ac01edf30ad1f45d9
tunogya <tunogya@qq.com>
2021-08-15 13:54:58 +0800

    fix: add WKDT

--- src/pages/Wkdt/index.js
+++ src/pages/Wkdt/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {Box} from "@chakra-ui/react";
+import {Box, Center, Heading, SimpleGrid, Stack} from "@chakra-ui/react";
 
 function Index() {
   return (
-    <Box pl={4} pr={4}>
-      WKDT Token
-    </Box>
+    <Center>
+      <Stack pl={4} pr={4} spacing={8} textAlign={"center"} maxW={"650"}>
+        <Heading>Meet WKDT - Wakanda<br/>Governance Token</Heading>
+        <Heading fontSize={"md"}>WKDT</Heading>
+        <Heading fontSize={"3xl"}>How you can get WKDT</Heading>
+        <Heading fontSize={"md"}>WKDT </Heading>
+        <Heading fontSize={"3xl"}>How to use WKDT</Heading>
+        <SimpleGrid minChildWidth="120px" spacing="12px">
+          <Box height="80px">
+            <Heading fontSize={"md"}>Hustle in Wakanda DAO</Heading>
+          </Box>
+          <Box height="80px">
+            <Heading fontSize={"md"}>Vote for platform upgrades</Heading>
+          </Box>
+          <Box height="80px">
+            <Heading fontSize={"md"}>Choose featured network</Heading>
+          </Box>
+          <Box height="80px">
+            <Heading fontSize={"md"}>Participate in moderation</Heading>
+          </Box>
+        </SimpleGrid>
+      </Stack>
+    </Center>
   );
 }
 
