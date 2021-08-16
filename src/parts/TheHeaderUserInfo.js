4746921855614168fd54080da5cf620e74ed5000
tunogya <tunogya@qq.com>
2021-08-16 13:47:53 +0800

    fix: delete Balance

--- src/parts/TheHeaderUserInfo.js
+++ src/parts/TheHeaderUserInfo.js
@@ -3,23 +3,16 @@ import {
   Heading,
   Menu,
   MenuButton,
-  MenuDivider,
   MenuItem,
   MenuList, Spinner,
-  Stack,
 } from "@chakra-ui/react";
 import React, {Suspense} from "react";
 import {useHistory} from "react-router-dom";
 import {useCurrentUser} from "../hooks/use-current-user";
-import {useWkdtBalance} from "../hooks/use-wkdt-balance";
-import {IDLE} from "../global/constants";
 
 export function TheHeaderUserInfo() {
   const history = useHistory();
-  const [user, loggedIn, {logOut}] = useCurrentUser()
-  const wkdt = useWkdtBalance(user.addr)
-
-  console.log(wkdt)
+  const [user, {logOut}] = useCurrentUser()
 
   return (
     <Menu isLazy={true}>
@@ -27,21 +20,6 @@ export function TheHeaderUserInfo() {
         {user.addr}
       </MenuButton>
       <MenuList>
-        <Stack ml={3} mr={3} spacing={2}>
-          <Stack>
-            <Heading fontSize="xs" color={"gray"}>My balance</Heading>
-            <Heading fontSize="sm">
-              {(wkdt.status !== IDLE || wkdt.status !== IDLE) && (
-                <Spinner size="sm"/>
-              )}
-              WKDT</Heading>
-          </Stack>
-          <Stack>
-            <Heading fontSize="xs" color={"gray"}>My locked balance</Heading>
-            <Heading fontSize="sm">200 WKDT</Heading>
-          </Stack>
-        </Stack>
-        <MenuDivider/>
         <MenuItem onClick={() => history.push("/wallet")}>
           <Heading fontSize="md">My wallet</Heading>
         </MenuItem>
