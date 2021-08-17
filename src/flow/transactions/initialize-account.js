bbe715552dcfc280cf03d52975d8554fd6367352
tunogya <tunogya@qq.com>
2021-08-17 22:11:56 +0800

    fix: update init tx

--- src/flow/transactions/initialize-account.js
+++ src/flow/transactions/initialize-account.js
@@ -12,28 +12,42 @@ import WakandaProfile from 0xWakandaProfile
 transaction {
   prepare(signer: AuthAccount) {
     if !WakandaToken.check(signer.address) {
-      signer.save(<-WakandaToken.createEmptyVault(), to: WakandaToken.TokenStoragePath)
-      signer.link<&WakandaToken.Vault{FungibleToken.Receiver}>(
-        WakandaToken.TokenPublicReceiverPath,
-        target: WakandaToken.TokenStoragePath
-      )
-      signer.link<&WakandaToken.Vault{FungibleToken.Balance}>(
+      if(signer.borrow<&WakandaToken.Vault>(from: WakandaToken.TokenStoragePath) == nil) {
+        signer.save(<-WakandaToken.createEmptyVault(), to: WakandaToken.TokenStoragePath)
+        signer.link<&WakandaToken.Vault{FungibleToken.Receiver}>(
+          WakandaToken.TokenPublicReceiverPath,
+          target: WakandaToken.TokenStoragePath
+        )
+        signer.link<&WakandaToken.Vault{FungibleToken.Balance}>(
           WakandaToken.TokenPublicBalancePath,
           target: WakandaToken.TokenStoragePath
-      )
+        )
+      }
     }
 
     if !WakandaPass.check(signer.address) {
-      let collection <- WakandaPass.createEmptyCollection() as! @WakandaPass.Collection
-      signer.save(<-collection, to: WakandaPass.CollectionStoragePath)
-      signer.link<&{NonFungibleToken.CollectionPublic, WakandaPass.CollectionPublic}>(
-        WakandaPass.CollectionPublicPath,
-        target: WakandaPass.CollectionStoragePath)
+      if signer.borrow<&WakandaPass.Collection>(from: WakandaPass.CollectionStoragePath) == nil {
+        let collection <- WakandaPass.createEmptyCollection() as! @WakandaPass.Collection
+        signer.save(<-collection, to: WakandaPass.CollectionStoragePath)
+        signer.link<&{NonFungibleToken.CollectionPublic, WakandaPass.CollectionPublic}>(
+            WakandaPass.CollectionPublicPath,
+            target: WakandaPass.CollectionStoragePath)
+      }
+      
+      if signer.borrow<&WakandaPass.NFTMinter>(from: WakandaPass.MinterStoragePath) == nil {
+        let minter <- WakandaPass.createNewMinter() as! @WakandaPass.NFTMinter
+        signer.save(<-minter, to: WakandaPass.MinterStoragePath)
+        signer.link<&{WakandaPass.MinterPublic}>(
+            WakandaPass.MinterPublicPath,
+            target: WakandaPass.MinterStoragePath)
+      }
     }
     
     if !WakandaProfile.check(signer.address) {
-      signer.save(<- WakandaProfile.new(), to: WakandaProfile.ProfileStoragePath)
-      signer.link<&WakandaProfile.WakandaProfileBase{WakandaProfile.WakandaProfilePublic}>(WakandaProfile.ProfilePublicPath, target: WakandaProfile.ProfileStoragePath)
+      if signer.borrow<&WakandaProfile.WakandaProfileBase>(from: WakandaProfile.ProfileStoragePath) ==nil {
+        signer.save(<- WakandaProfile.new(), to: WakandaProfile.ProfileStoragePath)
+        signer.link<&WakandaProfile.WakandaProfileBase{WakandaProfile.WakandaProfilePublic}>(WakandaProfile.ProfilePublicPath, target: WakandaProfile.ProfileStoragePath)
+      }
     }
   }
 }
