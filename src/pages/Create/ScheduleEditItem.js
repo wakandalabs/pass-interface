3b7841a8f7282c762f70c4e9c32a1bc9b955b61a
tunogya <tunogya@qq.com>
2021-08-20 16:52:29 +0800

    fix: update schedule item

--- src/pages/Create/ScheduleEditItem.js
+++ src/pages/Create/ScheduleEditItem.js
@@ -22,12 +22,13 @@ export function ScheduleEditItem({index, items, callback}) {
       <Box w={"50%"}>
         <Input id={"key"} placeholder="e.g. 2022.12.31 0:0:0" size="md" variant={"flushed"}
                onChange={(e) => handleDate(e.target.value)}/>
-        <Text fontSize={"xs"} color={"gray"}>{"(" + Date.parse(date) + ")"}</Text>
+        <Text fontSize={"xs"} color={"gray"}>{"time (" + Date.parse(date) + ")"}</Text>
       </Box>
       <Box w={"50%"}>
-        <Input id={"value"} placeholder="e.g. 100% or 1.0" size="md" variant={"flushed"}
+        <Input id={"value"} placeholder="0.0 - 1.0" size="md" variant={"flushed"}
+               isInvalid={percent<0 || percent > 1}
                onChange={(e) => handleValue(e.target.value)}/>
-        <Text fontSize={"xs"} color={"gray"}>{"(" + Number(percent) + ")"}</Text>
+        <Text fontSize={"xs"} color={"gray"}>{"(" + Number(percent) + ") wkdt remain locked"}</Text>
       </Box>
     </Stack>
   )
