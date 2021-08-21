b08b2af12109278214b0489f686990e22dbbc97d
tunogya <tunogya@qq.com>
2021-08-21 20:55:18 +0800

    fix: add script is init  storefront

---
+++ src/flow/script.is-storefront-init.js
@@ -0,0 +1,22 @@
+import {send, decode, script, args, arg, cdc} from "@onflow/fcl"
+import {Address} from "@onflow/types"
+
+const CODE = cdc`
+import WakandaStorefront from 0xWakandaStorefront
+
+pub fun main(address: Address): Bool {
+    return WakandaStorefront.check(address)
+}
+`
+
+export function scriptIsProfileInit(address) {
+  if (address == null) return Promise.resolve(false)
+
+  // prettier-ignore
+  return send([
+    script(CODE),
+    args([
+      arg(address, Address)
+    ])
+  ]).then(decode)
+}
