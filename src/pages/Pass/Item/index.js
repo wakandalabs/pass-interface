f371c52de79b82f4b8906e86b88e8eeeab71ee6d
tunogya <tunogya@qq.com>
2021-08-21 12:11:28 +0800

    fix: add prop

--- src/pages/Pass/Item/index.js
+++ src/pages/Pass/Item/index.js
@@ -1,34 +1,59 @@
 import React, {Suspense} from "react";
 import {useHistory} from "react-router-dom";
-import {Box, SkeletonText} from "@chakra-ui/react";
+import {Box, SkeletonText, Stack, Text} from "@chakra-ui/react";
 import {useCurrentUserHook} from "../../../hooks/use-current-user.hook";
+import {useWakandaPassDetail} from "../../../hooks/use-pass-detail.hook";
+import {fmtWkdt} from "../../../util/fmt-wkdt";
 
-export function PassItem(){
+export function PassItem() {
   const history = useHistory()
   const [cu, loggedIn] = useCurrentUserHook()
-  const id = history.location.pathname.slice(6)
+  const id = Number(history.location.pathname.slice(6))
+  const pass = useWakandaPassDetail(cu.addr, id)
 
-  if (!loggedIn){
+  console.log(pass)
+
+  if (!loggedIn) {
     return <PassItemSkeleton/>
   }
 
-  return(
-      <Box pl={4} pr={4} minH={"60vh"}>
-        {id}
-      </Box>
+  return (
+    <Stack pl={4} pr={4} minH={"60vh"}>
+      <Text>#{pass.pass.id}</Text>
+      <Text>idleBalance:{fmtWkdt(pass.pass.idleBalance, true)}</Text>
+      <Text>lockupAmount:{fmtWkdt(pass.pass.lockupAmount, true)}</Text>
+      {/*<Text>{pass.pass.lockupSchedule}</Text>*/}
+      {/*<Text>{pass.pass.metadata}</Text>*/}
+      <Text>originalOwner:{pass.pass.originalOwner}</Text>
+      <Text>owner:{pass.pass.owner}</Text>
+      {/*<Text>{pass.pass.stakingInfo}</Text>*/}
+      {/*<Text>{pass.pass.stamps}</Text>*/}
+      <Text>{fmtWkdt(pass.pass.totalBalance, true)}</Text>
+      <Text>VIP: {pass.pass.vipTier}</Text>
+
+
+
+
+
+
+
+
+
+
+    </Stack>
   )
 }
 
-export function PassItemSkeleton(){
-  return(
-    <Box pl={4} pr={4} minH={"60vh"}>
-      <SkeletonText />
-    </Box>
+export function PassItemSkeleton() {
+  return (
+    <Stack pl={4} pr={4} minH={"60vh"}>
+      <SkeletonText/>
+    </Stack>
   )
 }
 
-export default function WrappedPassItem(){
-  return(
+export default function WrappedPassItem() {
+  return (
     <Suspense fallback={<PassItemSkeleton/>}>
       <PassItem/>
     </Suspense>
