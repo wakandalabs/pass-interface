ab6645655962784a1592947c2c4690d46401e7cc
tunogya <tunogya@qq.com>
2021-08-21 12:16:01 +0800

    fix: update the footer

--- src/components/TheFooter.js
+++ src/components/TheFooter.js
@@ -1,4 +1,4 @@
-import {Divider, Grid, Heading, Link, Select, Spacer, Stack} from "@chakra-ui/react";
+import {Divider, Grid, Heading, Link, Select, Spacer, Stack, Text} from "@chakra-ui/react";
 import React, {Suspense} from "react";
 import {AiOutlineGithub, AiOutlineTaobao, AiOutlineTwitter} from "react-icons/all";
 
@@ -9,28 +9,28 @@ export function TheFooter() {
       <Grid templateColumns="repeat(4, 1fr)" gap={3} pb={20} pt={20}>
         Wakanda Labs
         <Stack w="100%" spacing={6}>
-          <Heading fontSize={"3xl"}>World of Wakanda</Heading>
-          <Heading fontSize={"xl"}>
+          <Heading fontSize={"md"}>World of Wakanda</Heading>
+          <Heading fontSize={"md"} color={"gray.500"}>
             <Link href={"/"}>Explore</Link>
           </Heading>
-          <Heading fontSize={"xl"}>
+          <Heading fontSize={"md"} color={"gray.500"}>
             <Link href={"/pass"}>My pass</Link>
           </Heading>
-          <Heading fontSize={"xl"}>
+          <Heading fontSize={"md"} color={"gray.500"}>
             <Link href={"/following"}>Following</Link>
           </Heading>
 
         </Stack>
         <Stack w="100%" spacing={6}>
-          <Heading fontSize={"3xl"}>Community</Heading>
-          <Heading fontSize={"xl"}>
+          <Heading fontSize={"md"}>Community</Heading>
+          <Heading fontSize={"md"} color={"gray.500"}>
             <Link href={"/wkdt"}>WKDT Token</Link>
           </Heading>
-          <Heading fontSize={"xl"}>Voting</Heading>
-          <Heading fontSize={"xl"}>Suggest feature</Heading>
+          <Heading fontSize={"md"} color={"gray.500"}>Voting</Heading>
+          <Heading fontSize={"md"} color={"gray.500"}>Suggest feature</Heading>
         </Stack>
         <Stack w="100%" spacing={3}>
-          <Heading fontSize={"3xl"}>Language</Heading>
+          <Heading fontSize={"md"}>Language</Heading>
           <Select variant="filled">
             <option value="english">English</option>
             {/*<option value="chinese">中文</option>*/}
@@ -39,7 +39,7 @@ export function TheFooter() {
       </Grid>
       <Divider/>
       <Stack direction={"row"} align={"center"} spacing={4}>
-        <Heading fontSize={"md"}>© Wakanda Labs. All rights reserved.</Heading>
+        <Text fontSize={"md"}>© Wakanda Labs. All rights reserved.</Text>
         <Spacer/>
         <Link href={"https://github.com/wakandalabs/"}><AiOutlineGithub size={20}/></Link>
         <Link href={"https://twitter.com/wakandalabs/"}><AiOutlineTwitter size={20}/></Link>
