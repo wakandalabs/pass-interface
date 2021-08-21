2ed1924a00d8cb9ad4e6cba850a8038ea30c1a66
tunogya <tunogya@qq.com>
2021-08-22 00:52:12 +0800

    fix: add use-storefront.hook.js

---
+++ src/hooks/use-storefront.hook.js
@@ -0,0 +1,37 @@
+import {atomFamily, selectorFamily, useRecoilState} from "recoil";
+import {fetchWakandaProfile} from "../flow/script.fetch-profile";
+import {ERROR, IDLE, IDLE_DELAY, PROCESSING, SUCCESS} from "../global/constants";
+import {sleep} from "../util/sleep";
+
+export const valueAtom = atomFamily({
+  key: address => address + "-storefront::state",
+  default: selectorFamily({
+    key: address => address + "-storefront::default",
+    get: address => async () => fetchWakandaProfile(address),
+  }),
+})
+
+export const statusAtom = atomFamily({
+  key: address => address + "-storefront::status",
+  default: IDLE,
+})
+
+export function useStorefrontHook(address) {
+  const [profile, setProfile] = useRecoilState(valueAtom(address))
+  const [status, setStatus] = useRecoilState(statusAtom(address))
+
+  async function refresh() {
+    setStatus(PROCESSING)
+    await fetchWakandaProfile(address).then(setProfile)
+    setStatus(IDLE)
+  }
+
+  return {
+    profile,
+    status,
+    refresh,
+    async buy(profile) {
+
+    },
+  }
+}
