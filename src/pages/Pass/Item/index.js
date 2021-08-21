daab7b53b4d4a284c2b3baac48588de2342f92c1
tunogya <tunogya@qq.com>
2021-08-21 11:33:38 +0800

    fix: get id from url

--- src/pages/Pass/Item/index.js
+++ src/pages/Pass/Item/index.js
@@ -1,17 +1,29 @@
 import React, {Suspense} from "react";
+import {useHistory} from "react-router-dom";
+import {Box, SkeletonText} from "@chakra-ui/react";
+import {useCurrentUserHook} from "../../../hooks/use-current-user.hook";
 
 export function PassItem(){
+  const history = useHistory()
+  const [cu, loggedIn] = useCurrentUserHook()
+  const id = history.location.pathname.slice(6)
+
+  if (!loggedIn){
+    return <PassItemSkeleton/>
+  }
+
   return(
-    <>
-      item
-    </>
+      <Box pl={4} pr={4} minH={"60vh"}>
+        {id}
+      </Box>
   )
 }
 
 export function PassItemSkeleton(){
   return(
-    <>
-    </>
+    <Box pl={4} pr={4} minH={"60vh"}>
+      <SkeletonText />
+    </Box>
   )
 }
 
