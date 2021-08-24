22baeb71fbb488173f0d9572df4245c296a74603
tunogya <tunogya@qq.com>
2021-08-25 02:09:27 +0800

    fix: bug

--- src/components/InitializeAlert.js
+++ src/components/InitializeAlert.js
@@ -1,10 +1,11 @@
-import {Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton, Link, Stack} from "@chakra-ui/react";
+import {Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton, Stack, Text} from "@chakra-ui/react";
 import React, {Suspense} from "react";
 import {useCurrentUserHook} from "../hooks/use-current-user.hook";
 import {useInitWkdtHook} from "../hooks/use-init-wkdt.hook";
 import {useInitProfileHook} from "../hooks/use-init-profile.hook";
 import {useInitPassHook} from "../hooks/use-init-pass.hook";
 import {useInitStorefrontHook} from "../hooks/use-init-store.hook";
+import {useHistory} from "react-router-dom";
 
 export function InitializeAlert() {
   const [cu, loggedIn] = useCurrentUserHook()
@@ -12,6 +13,7 @@ export function InitializeAlert() {
   const profile = useInitProfileHook(cu.addr)
   const pass = useInitPassHook(cu.addr)
   const storefront = useInitStorefrontHook(cu.addr)
+  const history = useHistory()
 
   if ((!loggedIn) || (loggedIn && wkdt.isInitialized && profile.isInitialized && pass.isInitialized && storefront.isInitialized)) {
     return <InitializeAlertSkeleton/>
@@ -22,9 +24,10 @@ export function InitializeAlert() {
       <AlertIcon />
       <AlertTitle mr={2}>Sorry, your account need to be initialized.</AlertTitle>
       <AlertDescription>
-        <Link color="teal.500" href={"/initialize"}>
+        <Text color="teal.500" onClick={()=> history.push("/initialize")}>
         Let's go!
-      </Link></AlertDescription>
+      </Text>
+      </AlertDescription>
       <CloseButton position="absolute" right="8px" top="8px" />
     </Alert>
   )
