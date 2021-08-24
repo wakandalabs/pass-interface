8c2bbdea0cd2ad100054483ae99dceb11f30a4e0
tunogya <tunogya@qq.com>
2021-08-25 02:05:17 +0800

    fix: bug

--- src/flow/script.fetch-sale-offer-ids.js
+++ src/flow/script.fetch-sale-offer-ids.js
@@ -7,14 +7,13 @@ import WakandaStorefront from 0xWakandaStorefront
 // This script returns an array of all the NFTs uuids for sale through a Storefront
 
 pub fun main(address: Address): [UInt64] {
-    let storefrontRef = getAccount(address)
+    if let storefrontRef = getAccount(address)
         .getCapability<&WakandaStorefront.Storefront{WakandaStorefront.StorefrontPublic}>(
             WakandaStorefront.StorefrontPublicPath
-        )
-        .borrow()
-        ?? panic("Could not borrow public storefront from address")
-    
-    return storefrontRef.getSaleOfferIDs()
+        ).borrow(){
+          return storefrontRef.getSaleOfferIDs()
+        }
+    return []
 }
 `
 
